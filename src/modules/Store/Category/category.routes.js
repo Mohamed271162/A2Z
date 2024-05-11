62d8e4813be1c521fb5d5ee1479109bce9627392
mohamed <mohamed.ashraf.kebal@gmail.com>
2024-05-11 12:02:25 +0200

    khod ya anas

--- src/modules/Store/Category/category.routes.js
+++ src/modules/Store/Category/category.routes.js
@@ -1,18 +1,28 @@
 import { Router } from 'express'
-// import { multerCloudFunction } from '../../services/multerCloud.js'
-// import { allowedExtensions } from '../../utils/allowedExtensions.js'
-// import { asyncHandler } from '../../utils/errorhandling.js'
-// import * as cc from './category.controller.js'
+import { multerCloudFunction } from '../../../services/multerCloud.js'
+import { allowedExtensions } from '../../../utils/allowedExtensions.js'
+import { asyncHandler } from '../../../utils/errorhandling.js'
+import * as cc from './category.controller.js'
 // import { validationCoreFunction } from '../../../middlewares/validation.js'
 // import * as validators from './category.validationSchemas.js'
 
 const router = Router()
 
 
-// router.post(
-//   '/',
-//   multerCloudFunction(allowedExtensions.Image).single('image'),
+router.post(
+  '/',
+  multerCloudFunction(allowedExtensions.Image).single('image'),
 //   validationCoreFunction(validators.createCategorySchema),
-//   asyncHandler(cc.createCategory),
-// )
+  asyncHandler(cc.createCategory),
+)
+router.put(
+    '/:categoryId',
+    multerCloudFunction(allowedExtensions.Image).single('image'),
+    // validationCoreFunction(validators.updateCategorySchema),
+    asyncHandler(cc.updateCategory),
+  )
+  
+  router.get('/', asyncHandler(cc.getAllCategories))
+  
+  router.delete('/', asyncHandler(cc.deleteCategory)) // TODO: api validation
 export default router
